0f79993e23deca4fa24edea02eec586a1301f024
mhkhizil <mhkhizilthurainzaw@gmail.com>
2023-08-18 22:44:38 +0630

    second commit

--- src/Pages/Media/Mediapgwpic.jsx
+++ src/Pages/Media/Mediapgwpic.jsx
@@ -5,14 +5,80 @@ import { FcCancel } from "react-icons/fc";
 import { BsImages, BsFillGridFill } from "react-icons/bs";
 import React, { useState } from "react";
 import { Table } from "@mantine/core";
-import {RiDeleteBin5Line} from "react-icons/ri";
-import {PiCopyDuotone} from "react-icons/pi"
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { PiCopyDuotone } from "react-icons/pi";
+import Breadcrumb from "../../Components/Breadcrumb";
 const elements = [
-  { No: 1, name: 'thurein zaw' , account: "C0123", date: "12/7/2023" ,time:"10:00 a.m",filesize:"20.0mb",action:<div className=" flex"><RiDeleteBin5Line/><PiCopyDuotone/></div>},
-  { No: 2, name: 'htetarkar zaw', account: "C678", date: "16/7/2023" ,time:"12:00 a.m",filesize:"20.0mb",action:<div className=" flex"><RiDeleteBin5Line/><PiCopyDuotone/></div>},
-  { No: 3, name: 'Min arkar zaw', account: "C7896", date: "18/72023" ,time:"1:00 a.m",filesize:"20.0mb",action:<div className=" flex"><RiDeleteBin5Line/><PiCopyDuotone/></div>},
-  { No: 4, name: 'htet nadi', account: "C5676", date: "1/72023" ,time:"10:00 p.m",filesize:"20.0mb",action:<div className=" flex"><RiDeleteBin5Line/><PiCopyDuotone/></div>},
-  { No: 5, name: 'Wai yan zaw', account: "C2345", date: "5/7/2023",time:"1:00 a.m",filesize:"20.0mb" ,action:<div className=" flex"><RiDeleteBin5Line/><PiCopyDuotone/></div>},
+  {
+    No: 1,
+    name: "thurein zaw",
+    account: "C0123",
+    date: "12/7/2023",
+    time: "10:00 a.m",
+    filesize: "20.0mb",
+    action: (
+      <div className="  flex">
+        <RiDeleteBin5Line className=" cursor-pointer hover:text-red-700" />
+        <PiCopyDuotone className="cursor-pointer hover:text-blue-700" />
+      </div>
+    ),
+  },
+  {
+    No: 2,
+    name: "htetarkar zaw",
+    account: "C678",
+    date: "16/7/2023",
+    time: "12:00 a.m",
+    filesize: "20.0mb",
+    action: (
+      <div className=" flex">
+        <RiDeleteBin5Line className=" cursor-pointer hover:text-red-700" />
+        <PiCopyDuotone className="cursor-pointer hover:text-blue-700" />
+      </div>
+    ),
+  },
+  {
+    No: 3,
+    name: "Min arkar zaw",
+    account: "C7896",
+    date: "18/72023",
+    time: "1:00 a.m",
+    filesize: "20.0mb",
+    action: (
+      <div className=" flex">
+        <RiDeleteBin5Line className=" cursor-pointer hover:text-red-700" />
+        <PiCopyDuotone className="cursor-pointer hover:text-blue-700" />
+      </div>
+    ),
+  },
+  {
+    No: 4,
+    name: "htet nadi",
+    account: "C5676",
+    date: "1/72023",
+    time: "10:00 p.m",
+    filesize: "20.0mb",
+    action: (
+      <div className=" flex">
+        <RiDeleteBin5Line className=" cursor-pointer hover:text-red-700" />
+        <PiCopyDuotone className="cursor-pointer hover:text-blue-700" />
+      </div>
+    ),
+  },
+  {
+    No: 5,
+    name: "Wai yan zaw",
+    account: "C2345",
+    date: "5/7/2023",
+    time: "1:00 a.m",
+    filesize: "20.0mb",
+    action: (
+      <div className=" flex">
+        <RiDeleteBin5Line className=" cursor-pointer hover:text-red-700" />
+        <PiCopyDuotone className="cursor-pointer hover:text-blue-700" />
+      </div>
+    ),
+  },
 ];
 
 const rows = elements.map((element) => (
@@ -62,8 +128,11 @@ const Mediapgwpic = (props) => {
   return (
     <div className="  ">
       <div className=" p-10">
-        <p className=" text-2xl text-white font-bold tracking-wide ">Media</p>
-        <p className="  pt-1 text-gray-300">Media/Uploader</p>
+        <Breadcrumb
+          title={"Media"}
+          firstRoute={"Media"}
+          secondRoute={"Uploader"}
+        />
         <Dropzone
           className=" mt-6 mb-10 bg-softblack  hover:bg-softblack border border-solid"
           onDrop={(files) => console.log("accepted files", files)}
@@ -96,18 +165,22 @@ const Mediapgwpic = (props) => {
             </div>
           </Group>
         </Dropzone>
-        <div className=" flex justify-between items-center">
-          <p className="pt-1 text-gray-300">
-            Media <span className=" text-blue-600">/</span> Uploaded photo
-          </p>
+        <div className=" pb-4 flex justify-between items-center">
+          <Breadcrumb firstRoute={"Media"} secondRoute={"Uploaded photo"} />
           <div className="">
             <AiOutlineOrderedList
               onClick={() => setDisplayState(true) && setDisplayState2(false)}
-              className={`${displayState?"text-blue-800":"text-gray-300"} ${displayState?"border-blue-800":"border-gray-300"} text-gray-300 border cursor-pointer border-solid border-gray-300 mx-2 inline`}
+              className={`${displayState ? "text-blue-800" : "text-gray-300"} ${
+                displayState ? "border-blue-800" : "border-gray-300"
+              } hover:text-blue-800 hover:border-blue-800 text-gray-300 border cursor-pointer border-solid border-gray-300 mx-2 inline`}
             />
             <BsFillGridFill
               onClick={() => setDisplayState(false) && setDisplayState2(true)}
-              className={`${!displayState?"text-blue-800":"text-gray-300"} ${!displayState?"border-blue-800":"border-gray-300"} text-gray-300  cursor-pointer border border-solid border-gray-300 inline`}
+              className={`${
+                !displayState ? "text-blue-800" : "text-gray-300"
+              } ${
+                !displayState ? "border-blue-800" : "border-gray-300"
+              } hover:text-blue-800 hover:border-blue-800 text-gray-300  cursor-pointer border border-solid border-gray-300 inline`}
             />
           </div>
         </div>
@@ -118,7 +191,7 @@ const Mediapgwpic = (props) => {
         >
           {images.map((i) => {
             return (
-              <img 
+              <img
                 className="  rounded-2xl w-[300px] h-[200px]"
                 src={i.link}
                 alt=""
@@ -126,7 +199,9 @@ const Mediapgwpic = (props) => {
             );
           })}
         </div>
-        <div className={`${!displayState ? "block" : "hidden"} overflow-y-auto`}>
+        <div
+          className={`${!displayState ? "block" : "hidden"} overflow-y-auto`}
+        >
           <Table>
             <thead>
               <tr>
