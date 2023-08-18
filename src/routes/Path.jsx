5b45d9ab368523bf85178bf086874c3e88f139bf
Hannah <erikahn05@gmail.com>
2023-08-19 03:20:27 +0630

    Auth Api, Login, Logout, Route Guard

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Dashboard from '../Pages/Dashboard'
-import UserList from '../Pages/user/UserList'
-import UserDetail from '../Pages/user/UserDetail'
-import CreateUser from '../Pages/user/CreateUser'
-import UpdateUser from '../Pages/user/UpdateUser'
-import Home from '../Pages/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Guard from "./Guard";
+import Dashboard from "../Pages/Dashboard";
+import UserList from "../Pages/user/UserList";
+import UserDetail from "../Pages/user/UserDetail";
+import CreateUser from "../Pages/user/CreateUser";
+import UpdateUser from "../Pages/user/UpdateUser";
+import Home from "../Pages/Home";
+import Login from "../Pages/Login/Login";
 
 export default function Path() {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home/>}>
-                <Route index element={<Dashboard/>}/>
-                <Route path='user-overview' element={<UserList/>}/>
-                <Route path='user-detail' element={<UserDetail/>}/>
-                <Route path='create-user' element={<CreateUser/>}/>
-                <Route path='update-user' element={<UpdateUser/>}/>
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route
+            index
+            element={
+              <Guard>
+                <Dashboard />
+              </Guard>
+            }
+          />
+          <Route
+            path="user-overview"
+            element={
+              <Guard>
+                <UserList />
+              </Guard>
+            }
+          />
+          <Route
+            path="user-detail"
+            element={
+              <Guard>
+                <UserDetail />
+              </Guard>
+            }
+          />
+          <Route
+            path="create-user"
+            element={
+              <Guard>
+                <CreateUser />
+              </Guard>
+            }
+          />
+          <Route
+            path="update-user"
+            element={
+              <Guard>
+                <UpdateUser />
+              </Guard>
+            }
+          />
+        </Route>
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
-  )
+  );
 }
