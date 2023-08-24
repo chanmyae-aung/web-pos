30d13a4bcae5a06359fe9c4350899ef92f6251aa
Hannah <erikahn05@gmail.com>
2023-08-24 09:57:27 +0630

    sale cashier ui fixed

--- src/Pages/Sale/Cashier.jsx
+++ src/Pages/Sale/Cashier.jsx
@@ -1,91 +1,80 @@
 import React from "react";
-import { Breadcrumbs, Link } from "@mui/material";
-import SaleCard from "../../Components/Sale/SaleCard";
-import NavBar from "../../Components/Sidebar/NavBar";
 import Reciept from "../../Components/Sale/Reciept";
+import { Breadcrumbs } from "@mantine/core";
+import { Link } from "@mui/material";
+import SaleCard from "../../Components/Sale/SaleCard";
 
 const Cashier = () => {
   return (
     <>
-     <NavBar/>
-    <div className=" flex gap-2 bg-[#202124]">
-       
-   <div className=" w-2/3 mt-14 px-5">
-     {/* sale breadcrumb */}
-     <div className=" pb-4  ">
-        <Breadcrumbs aria-label="breadcrumb" style={{ color: "#f5f5f5" }}>
-          <Link
-            sx={{ fontSize: "1rem" }}
-            href="#"
-            underline="hover"
-            color="#f5f5f5"
-          >
-            Sale
-          </Link>
-          <Link
-            sx={{ fontSize: "1rem" }}
-            underline="always"
-            color="#f5f5f5"
-            href="#"
-          >
-            Cashier
-          </Link>
-        </Breadcrumbs>
-      </div>
-      {/* sale tabs */}
-      <div className=" flex justify-between pb-3 border-b border-gray-700 ">
-        <div className="flex gap-5 mt-2">
-          <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
-            All
-          </a>
-          <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
-            Drinks
-          </a>
-          <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
-            Cakes
-          </a>
-          <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
-            Deserts
-          </a>
-          <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
-            Sets
-          </a>
+      <div className="bg-[#202124] text-[#f5f5f5] w-full h-screen overflow-auto ">
+        <h1 className="px-2 w-full py-2 border-b">POS CASHIER</h1>
+        <div className="flex gap-2">
+          {/* receipt section 1 (bought product list) */}
+
+          <div className="w-[65%]  h-full px-2 pt-3 ">
+            <Breadcrumbs aria-label="breadcrumb" style={{ color: "#f5f5f5" }}>
+              <Link
+                sx={{ fontSize: "1rem" }}
+                href="#"
+                underline="hover"
+                color="#f5f5f5"
+              >
+                Sale
+              </Link>
+              <Link
+                sx={{ fontSize: "1rem" }}
+                underline="always"
+                color="#f5f5f5"
+                href="#"
+              >
+                Cashier
+              </Link>
+            </Breadcrumbs>
+            <div className=" flex justify-between pb-3 border-b border-gray-700 ">
+              <div className="flex gap-5 mt-2">
+                <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
+                  All
+                </a>
+                <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
+                  Drinks
+                </a>
+                <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
+                  Cakes
+                </a>
+                <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
+                  Deserts
+                </a>
+                <a className="text-[#f5f5f5] hover:text-blue-500 active:text-blue-700">
+                  Sets
+                </a>
+              </div>
+              <input
+                id="exampleSearch2"
+                placeholder="search"
+                className=" w-44 px-2 py-1 rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding text-base font-normal leading-[1.6] text-white outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-white "
+                type="search"
+              />
+            </div>
+            {/* sale cards */}
+            <div className="px-1 flex flex-wrap gap-2 overflow-scroll  justify-center items-center">
+              
+              <SaleCard />
+              <SaleCard />
+              <SaleCard />
+              <SaleCard />
+              <SaleCard />
+              <SaleCard />
+            </div>
+          </div>
+
+          {/* reciept section 2 (calculator) */}
+          <div className="w-[35%] h-full border-l">
+            <Reciept calculator={true} printBtn={false} />
+          </div>
         </div>
-        <input
-          id="exampleSearch2"
-          placeholder="search"
-          className=" w-44 px-3 py-1 rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding text-base font-normal leading-[1.6] text-white outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-white "
-          type="search"
-        />
       </div>
-      {/* sale cards */}
-      <div className="px-2 flex flex-wrap gap-2  justify-center items-center mb-10">
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        <SaleCard/>
-        
-       
-
-        </div>
-      
-
-   </div>
-
-
-   <div className=" border-l mt-12   w-1/3">
-    <Reciept calculator={true} printBtn={false} />
-   </div>
-     
-    </div>
-    
     </>
-    
   );
 };
 
--- src/Pages/Sale/Checkout.jsx
+++ src/Pages/Sale/Checkout.jsx
@@ -3,20 +3,20 @@ import Reciept from "../../Components/Sale/Reciept";
 import NavBar from "../../Components/Sidebar/NavBar";
 import { NavLink } from "react-router-dom";
 import { Button, Typography } from "@mui/material";
-import {BsCashCoin,BsCreditCard,BsCreditCard2Back} from "react-icons/bs"
-import {HiArrowSmLeft} from "react-icons/hi"
+import { BsCashCoin, BsCreditCard, BsCreditCard2Back } from "react-icons/bs";
+import { HiArrowSmLeft } from "react-icons/hi";
 
-const Checkout = () => {
+const Checkout = ({ calculator, printBtn }) => {
   return (
     <>
       <NavBar />
       <div className=" flex gap-2 bg-[#202124]">
         <div className=" print:hidden w-2/3 mt-14 px-2">
-          <Typography
-            sx={{ fontSize: "0.8rem", paddingX: "5px" }}
-            gutterBottom
-          >
-            <NavLink to={"/sale-cashier"}> <HiArrowSmLeft className="mt-1 w-5 h-5"/> </NavLink>
+          <Typography sx={{ fontSize: "0.8rem", paddingX: "5px" }} gutterBottom>
+            <NavLink to={"/sale-cashier"}>
+              {" "}
+              <HiArrowSmLeft className="mt-1 w-5 h-5" />{" "}
+            </NavLink>
           </Typography>
           <Typography
             sx={{ fontSize: "1.7rem", paddingX: "10px" }}
@@ -25,27 +25,35 @@ const Checkout = () => {
             Payment Method
           </Typography>
           <div className="flex gap-5 my-3 py-5 border-b ">
-            <Button variant="outlined" sx={{color:"#f5f5f5",borderColor:"#3F4245"}} startIcon={<BsCashCoin />}>
+            <Button
+              variant="outlined"
+              sx={{ color: "#f5f5f5", borderColor: "#3F4245" }}
+              startIcon={<BsCashCoin />}
+            >
               Cash
             </Button>
-            <Button variant="outlined" sx={{color:"#f5f5f5",borderColor:"#3F4245"}} startIcon={<BsCreditCard />}>
+            <Button
+              variant="outlined"
+              sx={{ color: "#f5f5f5", borderColor: "#3F4245" }}
+              startIcon={<BsCreditCard />}
+            >
               Credit Card
             </Button>
-            <Button variant="outlined" sx={{color:"#f5f5f5",borderColor:"#3F4245"}} startIcon={<BsCreditCard2Back />}>
+            <Button
+              variant="outlined"
+              sx={{ color: "#f5f5f5", borderColor: "#3F4245" }}
+              startIcon={<BsCreditCard2Back />}
+            >
               Other
             </Button>
           </div>
-          <Typography
-            sx={{ fontSize: "2rem", paddingX: "10px" }}
-            gutterBottom
-          >
+          <Typography sx={{ fontSize: "2rem", paddingX: "10px" }} gutterBottom>
             <NavLink to={"/sale-cashier"}>Shipping</NavLink>
           </Typography>
           <div className="flex gap-5 px-5">
             <span>Pick-up</span>
             <span>Order</span>
           </div>
-          
         </div>
 
         <div className=" border-l mt-12 vendorListHeading print:bg-black   w-1/3">
